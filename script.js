c2f82036361094fdcd2d6fb33ef2e41e62a45fc3
Paul Brabet <pbrabet@gmail.com>
2017-04-20 14:54:56 +1200

    Clear button added. Beautified

--- script.js
+++ script.js
@@ -18,27 +18,34 @@ function getClickPosition(e) {
 canvas.addEventListener('click', getClickPosition, true);
 */
 
+var context = canvas.getContext('2d');
+
 // Draw circle function
 var draw = function () {
-
   // Create random colour
   var red = Math.floor(Math.random() * 256)
   var green = Math.floor(Math.random() * 256)
   var blue = Math.floor(Math.random() * 256)
   // Create randomn radius
   var circleRadius = Math.random() * 100;
-  //Create random location
+  // Create random location
   var xPos = Math.floor(Math.random() * width);
   var yPos = Math.floor(Math.random() * height);
+  // Draw circle
+  context.arc(xPos, yPos, circleRadius, 0, 2 * Math.PI);
+  context.lineWidth=1;
+  context.stroke();
+  context.fillStyle = 'rgba(' + red + ', ' + green + ', ' + blue + ', 0.5)';
+  context.fill()
 
-  if (canvas.getContext) {
-    var circle = canvas.getContext('2d');
-    //drawing code here
-    circle.arc(xPos, yPos, circleRadius, 0, 2 * Math.PI);
-    circle.fillStyle = 'rgba(' + red + ', ' + green + ', ' + blue + ', 0.5)';
-    circle.fill()
-  };
 };
+
+document.getElementById('button').addEventListener('click', function() {
+  // Making the canvas equal itself resets/redraws the entire canvas
+  canvas.width = canvas.width;
+  canvas.height = canvas.height;
+  },
+true);
 
 // draw();
 setInterval(draw, 200);
