6e73e61df52c3022f1ecebce9fa9be6936307da2
Paul Brabet <pbrabet@gmail.com>
2017-04-20 12:46:22 +1200

    Random circles generate

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ Bonus: Make notes semi random, so that they recreate popular progressions OR hav
 Bonus: Provide an option for different kinds of sounds
 Bonus: Have the circles shrink and grow based on its beat, peak size on beat, a dot when at half of beat*/
 
+var canvas = document.getElementById('c');
 var width = canvas.width;
 var height = canvas.height;
 
-var red = Math.floor(Math.random() * 256)
-var green = Math.floor(Math.random() * 256)
-var blue = Math.floor(Math.random() * 256)
+/* DISABLED BECAUSE IT DOESN'T SEEM TO WORK
+//  Obtain mouse location
+function getClickPosition(e) {
+  var xPos = e.clientX;
+  var yPos = e.clientY;
+};
+canvas.addEventListener('click', getClickPosition, true);
+*/
 
-var xPos = Math.random() * width;
-var yPos = Math.random() * width;
-var circleRadius = Math.random() * 50;
+// Draw circle function
+var draw = function () {
 
-function draw () {
-  var canvas = document.getElementById('canvas');
+  // Create random colour
+  var red = Math.floor(Math.random() * 256)
+  var green = Math.floor(Math.random() * 256)
+  var blue = Math.floor(Math.random() * 256)
+  // Create randomn radius
+  var circleRadius = Math.random() * 100;
+  //Create random location
+  var xPos = Math.floor(Math.random() * width);
+  var yPos = Math.floor(Math.random() * height);
+
   if (canvas.getContext) {
     var circle = canvas.getContext('2d');
     //drawing code here
@@ -27,7 +40,6 @@ function draw () {
   };
 };
 
-draw();
-
-
-//    ctx.moveTo(Math.random * width, Math.random * height);
+// draw();
+setInterval(draw, 200);
+// canvas.addEventListener('click', draw, false);
